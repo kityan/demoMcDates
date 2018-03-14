0da9b5fa9c6b596793a03ee0b3a70a04c9b57d1e
Pavel Kityan <pavel@kityan.ru>
2018-03-14 21:17:38 +0300

    $watch removed

--- app/mc-dates.component.ts
+++ app/mc-dates.component.ts
@@ -10,75 +10,125 @@ enum Presets {
 }
 
 class McDatesController implements ng.IComponentController {
-  public dateFrom: string;
-  public dateTo: string;
   public mcPlaceholder: string;
   public mcChange: (obj: object) => any;
-  private _dateFrom: Date | string;
-  private _dateTo: Date | string;
-  private moment: any;
+  private _dateFromObj: Date;
+  private _dateToObj: Date;
   private _presets: typeof Presets;
 
-  constructor(moment: any, $scope: any) {
+  private _sielence: boolean = false;
+  private _hasEventInSielence: boolean = false;
+
+  private moment: any;
+
+  constructor(moment: any) {
     this._presets = Presets;
     this.moment = moment;
-    $scope.$watchGroup(['$ctrl.dateFrom', '$ctrl.dateTo'], this._getBindings.bind(this));
-    $scope.$watchGroup(['$ctrl._dateFrom', '$ctrl._dateTo'], this._setBindings.bind(this));
   }
 
-  _getBindings(): void {
-    ['dateFrom', 'dateTo'].forEach((name: string) => {
-      let m: Moment = this.moment(this[name]);
-      this['_' + name] = (m.isValid()) ? m.toDate() : null;
-    });
+  private format(d: Date): string {
+    let m: Moment = this.moment(d);
+    return (m.isValid() ? m.format('YYYY-MM-DD') : null);
   }
 
-  _setBindings(newValues: Array<Date>, oldValues: Array<Date>, c): void {
-    let changed: boolean = false;
-    ['dateFrom', 'dateTo'].forEach((name: string) => {
-      let m: Moment = this.moment(this['_' + name]);
-      let prev: string = (this[name]) ? this[name] : null;
-      this[name] = (m.isValid()) ? m.format('YYYY-MM-DD') : null;
-      changed = (prev !== this[name]) ? true : changed;
-    });
-    if (changed) {
-      this.mcChange({ from: this._dateFrom, to: this._dateTo });
+  private parse(s: string): Date {
+    let m: Moment = this.moment(s);
+    return (m.isValid() ? m.toDate() : null);
+  }
+
+  // app <> mc-dates
+
+  public get dateFrom(): string {
+    return this.format(this._dateFromObj);
+  }
+
+  public set dateFrom(s: string) {
+    this.updateDateObject('_dateFromObj', s);
+  }
+
+  public get dateTo(): string {
+    return this.format(this._dateToObj);
+  }
+
+  public set dateTo(s: string) {
+    this.updateDateObject('_dateToObj', s);
+  }
+
+  // mc-dates <> md-datepicker
+
+  public get _dateFrom(): Date | string {
+    return this.format(this._dateFromObj);
+  }
+
+  public set _dateFrom(d: Date | string) {
+    this.updateDateObject('_dateFromObj', d, true);
+  }
+
+  public get _dateTo(): Date | string {
+    return this.format(this._dateToObj);
+  }
+
+  public set _dateTo(d: Date | string) {
+    this.updateDateObject('_dateToObj', d, true);
+  }
+
+  private updateDateObject(name: string, d: string | Date, emitChange?: boolean): void {
+    if (typeof d !== 'string') {
+      d = this.format(d);
     }
+    if (this.format(this[name]) !== d) {
+      this[name] = this.parse(d);
+      if (typeof this.mcChange === 'function' && emitChange) {
+        this.emitEvent();
+      }
+    }
+  }
 
+  emitEvent(): void {
+    if (!this._sielence) {
+      this.mcChange({ from: this.format(this._dateFromObj), to: this.format(this._dateToObj) });
+    } else {
+      this._hasEventInSielence = true;
+    }
   }
 
   applyPreset(preset: string | number): void {
     let m: Moment = this.moment();
+    let d: Array<Date>;
     switch (preset) {
       case this._presets.YESTERDAY:
         m.subtract('1', 'day');
-        this._dateFrom = m.toDate();
-        this._dateTo = m.toDate();
+        d = [m.toDate(), m.toDate()];
         break;
       case this._presets.TODAY:
-        this._dateFrom = m.toDate();
-        this._dateTo = m.toDate();
+        d = [m.toDate(), m.toDate()];
         break;
       case this._presets.TWOWEEKS:
-        this._dateTo = m.toDate();
-        this._dateFrom = m.subtract('14', 'day').toDate();
+        d = [m.toDate(), m.subtract('14', 'day').toDate()];
         break;
       case this._presets.MONTH:
-        this._dateTo = m.toDate();
-        this._dateFrom = m.subtract('1', 'month').toDate();
+        d = [m.toDate(), m.subtract('1', 'month').toDate()];
         break;
       case this._presets.ALL:
-        this._dateTo = null;
-        this._dateFrom = null;
+        d = [null, null];
         break;
     }
+    this._sielence = true; // disable emitting events
+    this._dateFrom = (d[1]) ? d[1] : null;
+    this._dateTo = (d[0]) ? d[0] : null;
+    this._sielence = false; // enable emitting events
+    if (this._hasEventInSielence) { // any event while disabled?
+      this._hasEventInSielence = false;
+      this.emitEvent();
+    }
+
   }
 }
 
 export class McDatesComponent implements ng.IComponentOptions {
 
   public static NAME: string = 'mcDates';
-  public static $inject: Array<string> = ['moment', '$scope'];
+  public static $inject: Array<string> = ['moment'];
   public bindings: any;
   public controller: any;
   public templateUrl: string;
@@ -92,6 +142,6 @@ export class McDatesComponent implements ng.IComponentOptions {
     };
     this.controller = McDatesController;
     this.templateUrl = require('./mc-dates.component.html');
-
   }
+
 }
